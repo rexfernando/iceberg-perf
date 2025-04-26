faa6fced14779c00bd6e17c3aceeb4c5e8a09574
rexfernando <rex_f@yahoo.com>
2025-04-25 17:50:17 -0700

    c

--- etc/trino/init.sql
+++ etc/trino/init.sql
@@ -32,5 +32,35 @@ INSERT INTO iceberg.tpch.synthetic_data
       CROSS JOIN iceberg.tpch.numbers b;
 
 
+-- Simple Scan
 SELECT count(*) FROM iceberg.tpch.synthetic_data;
+
+-- Filtered Scan
+SELECT count(*) FROM iceberg.tpch.synthetic_data WHERE value > 500;
+
+-- Aggregation
+SELECT name, avg(value) AS avg_value
+FROM iceberg.tpch.synthetic_data
+GROUP BY name
+LIMIT 1000;
+
+-- Complex filter and aggregation
+SELECT date_trunc('day', created_at) AS day,
+       count(*) AS record_count,
+       avg(value) AS avg_value
+FROM iceberg.tpch.synthetic_data
+WHERE value BETWEEN 200 AND 800
+GROUP BY date_trunc('day', created_at)
+ORDER BY day;
+
+-- Write Performance
+INSERT INTO iceberg.tpch.synthetic_data
+SELECT
+    row_number() OVER () + 10000000 AS id,
+    concat('user_', cast(row_number() OVER () + 10000000 AS VARCHAR)) AS name,
+    rand() * 1000 AS value,
+    current_timestamp AS created_at
+FROM iceberg.tpch.numbers a
+CROSS JOIN iceberg.tpch.numbers b
+LIMIT 1000000;
 
